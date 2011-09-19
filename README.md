3a7441711a9c3a3da4b792e9aaf5cf361fed8531
Brad Weslake <brad.weslake@gmail.com>
2011-09-18 23:47:47 -0400

    Sufficient functionality to release to Github.
    See README for information on the project.  Hopefully enough of the
    basic idea is now implemented to make the sources interesting to others.

--- README.md
+++ README.md
@@ -1,40 +1,114 @@
 # Maldini #
 
-Maldini is a [Nesta plugin][nestaplug] allowing citations and bibliographies to be automatically generated from a [BibTeX][bibtex] file. Maldini provides a syntax modeled on (a very small subset of) the syntax provided by [biblatex][biblatex].  Maldini is basically a wrapper for the excellent [bibtex-ruby][bruby] package by [Sylvester Keil][keil], which look after the hard work of parsing the BibTeX file.
+Maldini is a [Nesta plugin][nestaplug] allowing citations and bibliographies to be automatically generated from a [BibTeX][bibtex] file. Maldini provides a syntax modeled on (a very small subset of) the syntax provided by [biblatex][biblatex].  Maldini is basically a wrapper for [bibtex-ruby][bruby], which looks after the hard work of parsing the BibTeX file.
 
 Maldini is intended to streamline the workflow of those who write content both in [LaTeX][latex] and for the web.  It is also intended to make [Nesta][nesta] the tool of choice for academic websites.
 
-Maldini is an existence proof that a total amateur can easily build a useful Nesta plugin.  For comments and suggestions, please contact [the author][brad].
-
 ## Installation ##
+
+*Coming soon*. For now, see the [Nesta plugin][nestaplug] documentation.
 
 ## Usage ##
 
+*Coming soon*.
+
+## ACKNOWLEDGEMENTS ##
+
+- [Graham Ashton][ashton] for the lovely [Nesta][nesta].
+- [Sylvester Keil][keil] for the excellent [bibtex-ruby][bruby] and [citeproc-ruby][cruby] packages.
+- Philipp Lehman for the indispensable [biblatex][biblatex].
+
 ## VERSION HISTORY ##
+
+*Note*: Maldini is currently in pre-release development.  The first official release, of version 0.0.1, will occur only when a `gem` is released to [rubygems.org][rubygems.org].
 
 ### 0.0.1 ###
 
 - Initial skeleton release.
+- `Nesta::Plugin::Maldini::Bibliography` public methods:
+  - `open()`
+  - `textcite()`
+  - `citeauthor()`
+  - `parencite()`
+  - `fullcite()`
+  - `printbibliography()`
+- Supported entry types:
+  - `article`
+  - `book`
+  - `inbook`
+  - `incollection`
+- Unsupported entry types:
+  - `inproceedings`
+  - `unpublished`
+  - `phdthesis`
 
 ## TODO ##
 
+- Minor issues are flagged method by method in the source with "TODO".
+
+### Known Issues ###
+
+- Improve handling of various dashes in `pages` field. The best way to do this is probably to defer it to the [Haml][haml] processor, which [should][hamlredcarpet] in any case be using a [SmartyPants][smarty]-enabled processor such as [Redcarpet][redcarpet]. But it doesn't yet.
+- Better treatment of cases where a bibliography contains an author with multiple publications in a single year. Default behaviour should be to incrementally append a lowercase letter to the `year` field (Author, 2001a; Author, 2001b).
+- Reference list should be sorted alphabetically by default.
+
+### Wishlist ###
+
+- Find a way to embed citations directly in Markdown pages, perhaps by totally re-engineering everything to utilise the [citation processing][citepandoc] implemented by [Pandoc][pandoc].
+- Provide methods for citing page or section numbers, modeled on [biblatex][biblatex] syntax.
+- Provide methods for simultaneously citing multiple entries.
+- Add support for entry notes, including notes that include [biblatex][biblatex] citations to other entries.
+- Add support for DOI field, formatting as URI *iff* there is no URL field.
+- Automate testing with [RSpec][rspec] or something similar.
 - Allow BibTeX file to be sourced directly from a [GitHub][github] repository.
-- Implement option to format citations in any format specified in [Citation Style Language][csl], by wrapping [citeproc-ruby][cruby]
-- Add support for [Biber][biber]
+- Implement option to format citations in any format specified in [Citation Style Language][csl], by wrapping [citeproc-ruby][cruby].
+- Add support for [Biber][biber].
+
+## AUTHOR ##
+
+Maldini is an existence proof that a total amateur can easily build a useful Nesta plugin.  For comments and suggestions, please contact [the author][brad].
 
 ## LICENSE ##
 
-The Maldini plugin is distributed under an MIT license, as specified in file LICENSE.
+Copyright © 2011 Brad Weslake.
 
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+[ashton]: http://www.zerply.com/profile/grahamashton
 [biber]: http://biblatex-biber.sourceforge.net/
 [biblatex]: http://ctan.math.utah.edu/ctan/tex-archive/help/Catalogue/entries/biblatex.html
 [bibtex]: http://www.ctan.org/pkg/bibtex
 [brad]: http://bweslake.org/
 [bruby]: http://inukshuk.github.com/bibtex-ruby/
+[citepandoc]: http://johnmacfarlane.net/pandoc/README.html#citations
 [cruby]: https://github.com/inukshuk/citeproc-ruby
 [csl]: http://citationstyles.org/
 [github]: http://github.com/
+[hamlredcarpet]: https://github.com/nex3/haml/pull/383
+[haml]: http://haml-lang.com/
 [keil]: http://sylvester.keil.or.at/
 [latex]: http://www.latex-project.org/
 [nestaplug]: nestacms.com/docs/plugins/
-[nesta]: http://nestacms.com/[1]: http://
+[nesta]: http://nestacms.com/
+[pandoc]: http://johnmacfarlane.net/pandoc/index.html
+[redcarpet]: https://github.com/tanoku/redcarpet
+[rspec]: http://rspec.info/
+[smarty]: http://daringfireball.net/projects/smartypants/
+
+[rubygems.org]: rubygems.org
--- lib/nesta-plugin-maldini/init.rb
+++ lib/nesta-plugin-maldini/init.rb
@@ -1,4 +1,5 @@
 require 'bibtex'
+require 'date'
 
 module Nesta
   module Plugin
@@ -16,7 +17,7 @@ module Nesta
 
           # Initialise instance variables
           @thebibliography = nil
-          @theentry = nil
+          @theentries = []
 
           # Open file, if such there be
           if (file != nil)
@@ -38,8 +39,12 @@ module Nesta
         end # def open
 
         # CITATIONS
-        # The syntax is modeled on biblatex
+        # The syntax of citations is modeled on biblatex
         # See: http://goo.gl/UYoCY
+        #
+        # NOTES
+        # - Whenever a new entry is cited, it should be added to @theentries
+        #
         # TODO:
         # - Add starred versions, eg. textcite*(citekey), which suppress author names.
 
@@ -49,20 +54,249 @@ module Nesta
 
           # Assign an entry based on the citation key we have been passed.
           # TODO: Something smart if lookup fails.
-          @theentry = @thebibliography[citekey.to_sym]
+          theentry = @thebibliography[citekey.to_sym]
 
-          # @theentry[:author] returns an array of names.
-          # TODO: Display more than one surname, if such there be.
-          citestring = @theentry[:author][0].last + " (" + @theentry[:year].to_s + ")"
+          if theentry.has_field?(:crossref)
+            # The thing to do here is to populate any fields present in parent but not child.
+            # This is a hack for now.  See: https://github.com/inukshuk/bibtex-ruby/issues/22
+            theentry = crosspopulate(theentry,@thebibliography[theentry[:crossref].to_sym])
+          end # if
+
+          # Add to entries if not already present
+          @theentries << theentry if !@theentries.include?(theentry)
+
+          # BibTeX::Entry[:author] returns an array of BibTeX::Names.
+          theauthors = theentry[:author]
+          citestring = formatauthors(theauthors) + " (" + theentry[:year].to_s + ")"
           return citestring
         end # def textcite 
 
+        def citeauthor(citekey)
+          # USAGE: citeauthor('citationkey')
+          # Modeled on biblatex command \citeauthor{citationkey}
+
+          # We don't keep track of entries cited this way for displaying in the bibliography,
+          # so it may make sense to give a warning if this is called for an entry that is not
+          # already in @theentries
+
+          # Assign an entry based on the citation key we have been passed.
+          # TODO: Something smart if lookup fails.
+          theentry = @thebibliography[citekey.to_sym]
+
+          if theentry.has_field?(:crossref)
+            # The thing to do here is to populate any fields present in parent but not child.
+            # This is a hack for now.  See: https://github.com/inukshuk/bibtex-ruby/issues/22
+            theentry = crosspopulate(entry,@thebibliography[theentry[:crossref].to_sym])
+          end # if
+
+          # BibTeX::Entry[:author] returns an array of BibTeX::Names.
+          theauthors = theentry[:author]
+          citestring = formatauthors(theauthors)
+          return citestring                            
+        end # def citeauthor
+
         def parencite(citekey)
-        # USAGE: parencite('citationkey')
-        # Modeled on biblatex command \parencite{citationkey}
+          # USAGE: parencite('citationkey')
+          # Modeled on biblatex command \parencite{citationkey}
+          
+          # Assign an entry based on the citation key we have been passed.
+          # TODO: Something smart if lookup fails.
+          theentry = @thebibliography[citekey.to_sym]
+
+          if theentry.has_field?(:crossref)
+            # The thing to do here is to populate any fields present in parent but not child.
+            # This is a hack for now.  See: https://github.com/inukshuk/bibtex-ruby/issues/22
+            theentry = crosspopulate(entry,@thebibliography[theentry[:crossref].to_sym])
+          end # if
+
+          # Add to entries if not already present
+          @theentries << theentry if !@theentries.include?(theentry)
+
+          # BibTeX::Entry[:author] returns an array of BibTeX::Names.
+          theauthors = theentry[:author]
+          citestring = "(" + formatauthors(theauthors) + ", " + theentry[:year].to_s + ")"
+          return citestring
         end # def parencite
 
+        def fullcite(citekey)
+          # USAGE: fullcite('citationkey')
+          # Modeled on biblatex command \fullcite{citationkey}
+
+          # Assign an entry based on the citation key we have been passed.
+          # TODO: Something smart if lookup fails.
+          theentry = @thebibliography[citekey.to_sym]
+
+          if theentry.has_field?(:crossref)
+            # The thing to do here is to populate any fields present in parent but not child.
+            # This is a hack for now.  See: https://github.com/inukshuk/bibtex-ruby/issues/22
+            theentry = crosspopulate(entry,@thebibliography[theentry[:crossref].to_sym])
+          end # if
+
+          # Add to entries if not already present
+          @theentries << theentry if !@theentries.include?(theentry)
+
+          citestring = formatentry(theentry)
+          return citestring
+        end
+        
         # REFERENCE LISTS
+        def printbibliography()
+          # USAGE: printbibliography()
+          # Modeled on biblatex command \printbibliography
+          bibliographystring = ""
+          @theentries.each do |e|
+              bibliographystring << "- "
+              bibliographystring << formatentry(e)
+              bibliographystring << ".  \n"
+          end # each
+          return bibliographystring
+        end # def printbibliography
+
+        # PROTECTED METHODS
+        protected        
+
+        # FORMATS
+        def formatauthors(author)
+          # Returns last names in a format suitable for citations
+          # TODO:
+          # - There has to be a way to do this in a few elegant lines, but I am a hack  
+          # - Implement this in a class extending BibTeX::Names
+          citestring = ""
+          if (author.length > 0)
+            citestring << author[0].prefix.to_s unless author[0].prefix.nil?
+            citestring << " " << author[0].last
+            if (author.length > 1)
+              if (author.length > 2)
+                author[1,author.length-2].each do |a|
+                  citestring << ", "
+                  citestring << a.prefix.to_s unless a.prefix.nil?
+                  citestring << a.last
+                end # each
+              end # if
+              citestring << " and "
+              citestring << author[-1].prefix.to_s unless author[-1].prefix.nil?
+              citestring << author[-1].last
+            end # if
+          else # ie. author.length < 0
+            # TODO: Something smart here.
+          end # if
+          return citestring
+        end # def formatauthors
+
+        def formatentry(entry)
+          # USAGE: formatentry(BibTeX::Entry)        
+          # The format here is presently tailored to my own preferences. Formats should eventually be configurable.
+          # TODO:
+          # - Implement option to format citations in any format specified in Citation Style Language,
+          #   by wrapping citeproc-ruby.
+          entrystring = ""
+
+          # REQUIRED_FIELDS are specified in BibTeX::Entry::REQUIRED_FIELDS
+          # We assume these are all present.  All other fields we check before processing.
+
+          # ARTICLE
+          if entry.type == :article
+            # REQUIRED_FIELDS: :article => [:author,:title,:journal,:year]
+            entrystring << entry[:author].to_s
+            entrystring << "." if entrystring[-1] != "."
+            entrystring << " " << entry[:year].to_s << ". \"" << entry[:title].to_s + "\", in *" << entry[:journal].to_s << "*"
+            entrystring << ", Vol. " << entry[:volume].to_s if entry.has_field?(:volume)
+            entrystring << ", No. " << entry[:number].to_s if entry.has_field?(:number)
+            entrystring << ", " << Date::MONTHNAMES[BibTeX::Entry::MONTHS.index(entry[:month].to_sym)+1] if entry.has_field?(:month)
+            entrystring << ", pp. " << entry[:pages].to_s if entry.has_field?(:pages)
+            entrystring << ". URI: [" << entry[:url].to_s << "](" << entry[:url].to_s << ")" if entry.has_field?(:url)
+            # entrystring << "  \nFields: " << entry.fields.to_s
+
+          # BOOK
+          elsif entry.type == :book
+            # REQUIRED_FIELDS: :book => [[:author,:editor],:title,:publisher,:year]
+            
+            if entry.has_field?(:author)
+              entrystring << entry[:author].to_s
+            elsif entry.has_field?(:editor)
+              # TODO: Add (Ed) or (Eds) here
+              entrystring << entry[:editor].to_s
+            end
+            entrystring << "." if entrystring[-1] != "."
+            entrystring << " " << entry[:year].to_s << ". *" << entry[:title].to_s + "*"
+            entrystring << ", " << entry[:publisher].to_s if entry.has_field?(:publisher)
+            entrystring << ", " << entry[:address].to_s if entry.has_field?(:address)
+            entrystring << ", Vol. " << entry[:volume].to_s if entry.has_field?(:volume)
+            entrystring << ", No. " << entry[:number].to_s if entry.has_field?(:number)
+            entrystring << ", " << Date::MONTHNAMES[BibTeX::Entry::MONTHS.index(entry[:month].to_sym)+1] if entry.has_field?(:month)
+            entrystring << ". URI: [" << entry[:url].to_s << "](" << entry[:url].to_s << ")" if entry.has_field?(:url)
+            # entrystring << "  \nFields: " << entry.fields.to_s
+            
+          # INBOOK
+          elsif entry.type == :inbook
+            # REQUIRED_FIELDS: :inbook => [[:author,:editor],:title,[:chapter,:pages],:publisher,:year]
+
+            if entry.has_field?(:author)
+              entrystring << entry[:author].to_s
+            elsif entry.has_field?(:editor)
+              # TODO: Add (Ed) or (Eds) here
+              entrystring << entry[:editor].to_s
+            end
+            entrystring << "." if entrystring[-1] != "."
+            entrystring << " " << entry[:year].to_s << ". \"" << entry[:title].to_s + "\", in "
+            entrystring << "*" << entry[:booktitle].to_s << "*"
+            entrystring << ", " << entry[:publisher].to_s if entry.has_field?(:publisher)
+            entrystring << ", " << entry[:address].to_s if entry.has_field?(:address)
+            entrystring << ", Vol. " << entry[:volume].to_s if entry.has_field?(:volume)
+            entrystring << ", No. " << entry[:number].to_s if entry.has_field?(:number)
+            entrystring << ", " << Date::MONTHNAMES[BibTeX::Entry::MONTHS.index(entry[:month].to_sym)+1] if entry.has_field?(:month)
+            entrystring << ", pp. " << entry[:pages].to_s if entry.has_field?(:pages)
+            entrystring << ". URI: [" << entry[:url].to_s << "](" << entry[:url].to_s << ")" if entry.has_field?(:url)
+
+          # INCOLLECTION
+          elsif entry.type == :incollection
+            # REQUIRED_FIELDS: :incollection => [:author,:title,:booktitle,:publisher,:year]
+
+            entrystring << entry[:author].to_s << ". " << entry[:year].to_s << ". \"" << entry[:title].to_s + "\", in "
+            # editors
+            entrystring << "*" << entry[:booktitle].to_s << "*"
+            entrystring << ", " << entry[:publisher].to_s if entry.has_field?(:publisher)
+            entrystring << ", " << entry[:address].to_s if entry.has_field?(:address)
+            entrystring << ", Vol. " << entry[:volume].to_s if entry.has_field?(:volume)
+            entrystring << ", No. " << entry[:number].to_s if entry.has_field?(:number)
+            entrystring << ", " << Date::MONTHNAMES[BibTeX::Entry::MONTHS.index(entry[:month].to_sym)+1] if entry.has_field?(:month)
+            entrystring << ", pp. " << entry[:pages].to_s if entry.has_field?(:pages)
+            entrystring << ". URI: [" << entry[:url].to_s << "](" << entry[:url].to_s << ")" if entry.has_field?(:url)
+
+          # INPROCEEDINGS
+          elsif entry.type == :inproceedings
+            # REQUIRED_FIELDS: :inproceedings => [:author,:title,:booktitle,:year]
+            entrystring << "Maldini support for inproceedings coming soon"
+
+          # PHDTHESIS
+          elsif entry.type == :phdthesis
+            # REQUIRED_FIELDS: :phdthesis => [:author,:title,:school,:year]
+            #
+            # TODO:
+            # - Write this method.
+            entrystring << "Maldini support for phdthesis coming soon"
+
+          # UNPUBLISHED
+          elsif entry.type == :unpublished
+            # REQUIRED_FIELDS: :unpublished => [:author,:title,:note]
+            #
+            # TODO:
+            # - Write this method.
+            entrystring << "Maldini support for unpublished coming soon"
+
+          # OTHER
+          else
+            # TODO: Something smart here.
+          end # if
+          return entrystring
+        end #formatentry
+        
+        def crosspopulate(entry,parententry)
+          parententry.fields.keys.each do |k|
+            entry[k] = parententry[k] unless entry.has_field?(k)
+          end # each
+          return entry
+        end # def crosspopulate
         
       end # class Bibliography    
     end # module Maldini
@@ -71,4 +305,5 @@ module Nesta
   class App
     helpers Nesta::Plugin::Maldini::Helpers
   end # class App
+
 end # module Nesta
